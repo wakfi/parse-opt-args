2e660928c866b317da027c48e48bd178aa371828
Walker Gray <55608093+wakfi@users.noreply.github.com>
2022-03-26 17:03:58 -0700

    Add index and update package.json

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import parseOptArgs from "./parseOptArgs";
+import parsePositionalOptArgs from "./parsePositionalOptArgs";
+import parseTruthyOptArgs from "./parseTruthyOptArgs";
+
+export default parseOptArgs
+export {
+    parseOptArgs,
+    parsePositionalOptArgs,
+    parseTruthyOptArgs,
+}
